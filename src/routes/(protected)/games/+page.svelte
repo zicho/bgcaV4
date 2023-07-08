e5afd145ed9aea1abe006a0b3228a609afcd912f
Martin Ström <martin@datanist.se>
2023-07-08 16:05:08 +0200

    minor changes

--- src/routes/(protected)/games/+page.svelte
+++ src/routes/(protected)/games/+page.svelte
@@ -16,52 +16,58 @@
 	<PageHeaderToolbarButton displayText="Import collection" url="/games/import" icon="fa-download" />
 </PageHeaderToolbar>
 
-<div class="grid grid-cols-8 border border-base-200">
-	<!-- Column headers -->
-
-	<div
-		class="col-span-8 grid grid-cols-8 font-semibold border-b border-base-200 bg-primary-content"
+{#if data.games.length == 0}
+	<span
+		>No games yet. <a class="link" href="/games/import">Import your collection</a> or add games manually.</span
 	>
-		<div class="p-4 col-span-1" />
-		<div class="p-4 col-span-1">Title</div>
-		<div class="p-4 col-span-4">About</div>
-		<div class="p-4 col-span-1">Rating</div>
-		<div class="p-4 col-span-1" />
-	</div>
+{:else}
+	<div class="grid grid-cols-8 border border-base-200">
+		<!-- Column headers -->
 
-	{#each data.games as game}
-		<a
-			href="/games/{game.bggId}/{game.slug}"
-			class="col-span-8 grid grid-cols-8 border-base-200 border-b hover:bg-base-200 cursor-pointer"
+		<div
+			class="col-span-8 grid grid-cols-8 font-semibold border-b border-base-200 bg-primary-content"
 		>
-			<div class="p-4 col-span-1 flex justify-center items-center">
-				<img alt="Image of {game.name} box cover art" class="h-32 w-32" src={game.thumbnail} />
-			</div>
-			<div class="p-4 col-span-1 flex items-center">
-				<div class="flex flex-col">
-					<div class="font-bold overflow-hidden">{game.name}</div>
-					<div class="text-sm opacity-50 overflow-hidden">{game.yearPublished}</div>
+			<div class="p-4 col-span-1" />
+			<div class="p-4 col-span-1">Title</div>
+			<div class="p-4 col-span-4">About</div>
+			<div class="p-4 col-span-1">Rating</div>
+			<div class="p-4 col-span-1" />
+		</div>
+
+		{#each data.games as game}
+			<a
+				href="/games/{game.bggId}/{game.slug}"
+				class="col-span-8 grid grid-cols-8 border-base-200 border-b hover:bg-base-200 cursor-pointer"
+			>
+				<div class="p-4 col-span-1 flex justify-center items-center">
+					<img alt="Image of {game.name} box cover art" class="h-32 w-32" src={game.thumbnail} />
 				</div>
-			</div>
-			<div class="p-4 col-span-4 flex items-center">
-				<span>
-					{#if game.desc}
-						{game.desc}
-					{:else}
-						<i>No description yet for this game.</i>
-					{/if}
-				</span>
-			</div>
-			<div class="p-4 col-span-1 flex items-center">
-				<span>{game.rating}</span>
-			</div>
-			<div class="p-4 col-span-1 flex items-center justify-center">
-				<a
-					href="https://boardgamegeek.com/boardgame/{game.bggId}"
-					target="_blank"
-					class="btn btn-secondary"><i class="fa fa-external-link" /> View on BGG</a
-				>
-			</div>
-		</a>
-	{/each}
-</div>
+				<div class="p-4 col-span-1 flex items-center">
+					<div class="flex flex-col">
+						<div class="font-bold overflow-hidden">{game.name}</div>
+						<div class="text-sm opacity-50 overflow-hidden">{game.yearPublished}</div>
+					</div>
+				</div>
+				<div class="p-4 col-span-4 flex items-center">
+					<span>
+						{#if game.desc}
+							{game.desc}
+						{:else}
+							<i>No description yet for this game.</i>
+						{/if}
+					</span>
+				</div>
+				<div class="p-4 col-span-1 flex items-center">
+					<span>{game.rating}</span>
+				</div>
+				<div class="p-4 col-span-1 flex items-center justify-center">
+					<a
+						href="https://boardgamegeek.com/boardgame/{game.bggId}"
+						target="_blank"
+						class="btn btn-secondary"><i class="fa fa-external-link" /> View on BGG</a
+					>
+				</div>
+			</a>
+		{/each}
+	</div>
+{/if}
--- src/routes/(protected)/games/[bggId]/[name]/+page.svelte
+++ src/routes/(protected)/games/[bggId]/[name]/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import PageHeaderToolbar from '$lib/components/ui/PageHeaderToolbar.svelte';
 	import PageHeaderToolbarButton from '$lib/components/ui/PageHeaderToolbarButton.svelte';
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
 
-<PageHeaderToolbar title="{data.game?.name}" subheader="{data.game?.yearPublished?.toString()}">
-    <PageHeaderToolbarButton displayText="Add to collection" url="/events/create" icon="fa-plus" />
+<PageHeaderToolbar title={data.game?.name} subheader={data.game?.yearPublished?.toString()}>
+	<PageHeaderToolbarButton displayText="Add to collection" url="/events/create" icon="fa-plus" />
 	<PageHeaderToolbarButton displayText="Create event" url="/events/create" icon="fa-calendar" />
 </PageHeaderToolbar>
 
-{data.game?.desc}
+<div class="grid grid-cols-3">
+	<div class="col-span-3 md:col-span-1 flex justify-center items-center ">
+		<img class="aspect-square border border-primary" alt="{data.game?.name} box art" src={data.game?.imageUrl} />
+	</div>
+	<div class="md:px-4 col-span-3 md:col-span-2 mt-4 md:mt-0">
+		<p>{data.game?.desc}</p>
+	</div>
+</div>
