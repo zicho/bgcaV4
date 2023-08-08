b858c98e4cde621bea4d3a951ca7fa20b410aab0
Martin Ström <martin@datanist.se>
2023-08-08 20:55:59 +0200

    merge to master

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -20,8 +20,8 @@
 	<PageHeaderToolbarButton displayText="Import collection" url="/games/import" icon="fa-download" />
 </PageHeaderToolbar>
 
-<section class="grid md:grid-cols-2 md:gap-16 prose">
-	<article>
+<section class="grid md:grid-cols-2 md:gap-16">
+	<article class="prose">
 		<h2>Greetings, {data.user.username}!</h2>
 
 		<p>
@@ -36,14 +36,19 @@
 			<a href="/contact">contact me!</a>
 		</p>
 	</article>
-	<article class="md:col-span-1">
-		<h2>Latest registered users:</h2>
-
-		<p>Newly registered users. Say hello!</p>
-		<ul class="px-0 list-none">
-			{#each data.users as user}
-				<li class="px-0"><a href="/profile/{user.username}">{user.username}</a></li>
-			{/each}
-		</ul>
+	<article class="md:col-span-1 mt-8 md:mt-0">
+		<div class="card p-4 bg-base-100 shadow-md h-full">
+			<h2 class="mb-4 text-xl font-medium">Most recently registered users. Say hello!</h2>
+			<ul class="space-y-2 list-disc ml-4">
+				{#each data.users as user}
+					<li class="px-0 text-md font-medium hover:underline">
+						<a href="/profile/{user.username}"
+							>{user.username}
+							{#if user.username === data.user.username}(you){/if}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		</div>
 	</article>
 </section>
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,24 +3,27 @@ import { loadFlash, redirect } from "sveltekit-flash-message/server";
 import type { User } from "lucia";
 
 export const load: LayoutServerLoad = loadFlash(async (event) => {
-	const { locals, route, url } = event;
 
+	const { locals, route, url } = event;
 	const session = await locals.auth.validate();
 
 	if (route?.id?.includes("(protected)")) {
 		if (!session) {
 			const redirectToUrl = url.pathname + url.search;
-			console.dir(redirectToUrl);
 
-			throw redirect(
-				302,
-				redirectToUrl.length > 1 ? `/login?redirect=${redirectToUrl}` : "/login",
-				{
-					type: "error",
-					message: "You must be logged in to access this resource"
-				},
-				event
-			);
+			if (redirectToUrl.length > 1) {
+				throw redirect(
+					302,
+					`/login?redirect=${redirectToUrl}`,
+					{
+						type: "error",
+						message: "You must be logged in to access this resource"
+					},
+					event
+				);
+			}
+
+			throw redirect(302, `/login`);
 		}
 	}
 
