8d371a4da29063ccdde8c961967f27d7e2a18770
Martin Ström <martin@datanist.se>
2023-08-08 18:04:22 +0200

    modified:   src/lib/components/ui/Navbar.svelte

--- src/lib/components/ui/Navbar.svelte
+++ src/lib/components/ui/Navbar.svelte
@@ -13,13 +13,6 @@
 		authOnly: boolean;
 	}
 
-	import { writable } from "svelte/store";
-
-	const store = writable([]);
-	$store.length; // incorrect no-unsafe-member-access error
-
-	export let name: string;
-
 	// reactive to changes in user state
 	$: navbarLinks = [
 		{
