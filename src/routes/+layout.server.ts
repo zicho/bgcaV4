7b0cde94d9c710034f3cc9e337b2ce55368093ea
Martin Ström <martin@datanist.se>
2023-08-08 20:56:33 +0200

    Squashed commit of the following:
    commit b858c98e4cde621bea4d3a951ca7fa20b410aab0
    Author: Martin Ström <martin@datanist.se>
    Date:   Tue Aug 8 20:55:59 2023 +0200
    
        merge to master
    
    commit a19eef5a16b462ad9687793f6b141b6fa97c5aa9
    Author: Martin Ström <martin@datanist.se>
    Date:   Tue Aug 8 20:55:51 2023 +0200
    
        various
    
    commit db0c6aa60b989ba7a8e597b2b3c5e5621a48b7f9
    Author: Martin Ström <martin@datanist.se>
    Date:   Tue Aug 8 19:29:34 2023 +0200
    
        tightening up
    
    commit ec790bf5386c4eea40bc62e197a65aed6dd8b248
    Author: Martin Ström <martin@datanist.se>
    Date:   Tue Aug 8 19:28:04 2023 +0200
    
        handle redirects better
    
    commit f72395927fa953e72c5e290e115bd6ae2122e927
    Author: Martin Ström <martin@datanist.se>
    Date:   Tue Aug 8 19:18:44 2023 +0200
    
        fixed package updates
    
    commit ef9f2b467eb5917726bb97a7b8be4228b6a0a5ee
    Author: Martin Ström <martin@datanist.se>
    Date:   Tue Aug 8 19:18:35 2023 +0200
    
        fixes
    
    commit f7dd61f8cb0bbdf0e0eefb6516634033a1f98370
    Author: Martin Ström <martin@datanist.se>
    Date:   Tue Aug 8 18:26:58 2023 +0200
    
        last push before major update

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,14 +1,30 @@
-import { redirect } from "@sveltejs/kit";
-import { handleLoginRedirect } from "$lib/functions/handleLoginRedirect";
 import type { LayoutServerLoad } from "./$types";
-import { loadFlashMessage } from "sveltekit-flash-message/server";
+import { loadFlash, redirect } from "sveltekit-flash-message/server";
 import type { User } from "lucia";
 
-export const load: LayoutServerLoad = loadFlashMessage(async ({ locals, route, url }) => {
+export const load: LayoutServerLoad = loadFlash(async (event) => {
+
+	const { locals, route, url } = event;
 	const session = await locals.auth.validate();
 
 	if (route?.id?.includes("(protected)")) {
-		if (!session) throw redirect(302, handleLoginRedirect(url));
+		if (!session) {
+			const redirectToUrl = url.pathname + url.search;
+
+			if (redirectToUrl.length > 1) {
+				throw redirect(
+					302,
+					`/login?redirect=${redirectToUrl}`,
+					{
+						type: "error",
+						message: "You must be logged in to access this resource"
+					},
+					event
+				);
+			}
+
+			throw redirect(302, `/login`);
+		}
 	}
 
 	return { user: session?.user as User };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,11 @@
 	import "../app.css";
 	import type { PageData } from "./(protected)/$types";
 	import { realtimeStore } from "$lib/stores/realTimeStore";
-	import { initFlash } from "sveltekit-flash-message/client";
+	import { getFlash } from "sveltekit-flash-message/client";
 	import { page } from "$app/stores";
-	import { Toaster, toast } from "svelte-sonner";
+	import { Toaster } from "svelte-sonner";
 	import FlashMessage from "$lib/components/ui/FlashMessage.svelte";
-	import { afterNavigate } from "$app/navigation";
+	import { beforeNavigate } from "$app/navigation";
 	import Navbar from "$lib/components/ui/Navbar.svelte";
 
 	export let data: PageData;
@@ -14,45 +14,21 @@
 	$: ({ user } = data);
 	$: data.user && realtimeStore.sub(data.user?.username);
 
-	const flash = initFlash(page);
+	const flash = getFlash(page);
 
-	afterNavigate((nav) => {
+	beforeNavigate((nav) => {
 		if ($flash && nav.from?.url.toString() !== nav.to?.url.toString()) {
 			$flash = undefined;
 		}
 	});
-
-	$: {
-		if ($flash?.message) {
-			// todo: style the warning and info alerts if they are gonna be used
-			switch ($flash?.type) {
-				case "success":
-					toast.success($flash?.message as string);
-					break;
-				case "info":
-					toast($flash?.message as string);
-					break;
-				case "warning":
-					toast($flash?.message as string);
-					break;
-				case "error":
-					toast.error($flash?.message as string);
-					break;
-				default:
-					toast($flash?.message as string);
-			}
-		}
-	}
 </script>
 
 <Toaster richColors closeButton />
 <Navbar {user} />
 
-<noscript>
-	{#if $flash}
-		<FlashMessage message={$flash.message} type={$flash.type} />
-	{/if}
-</noscript>
+{#if $flash}
+	<FlashMessage message={$flash.message} type={$flash.type} />
+{/if}
 
 <div class="py-6 lg:py-10 px-6 lg:px-16">
 	<slot />
